df836a58dc831af4edfe2913903eea3092c52613
therealemissions <me@liammartin.site>
2023-01-01 12:42:45 +0000

    updated

--- out/types/Dependencies.d.ts
+++ out/types/Dependencies.d.ts
@@ -4,5 +4,5 @@ import { HeadFile } from "ts-modular-bot-file-design";
 export interface InjectWith {
     injectWith: Dependency | null;
 }
-export declare const vars: Map<Dependency, [HeadFile, string][]>;
+export declare const vars: [HeadFile, string][];
 export declare function inject(type: Dependency): (target: any, key: string) => void;
