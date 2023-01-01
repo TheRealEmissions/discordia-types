7d6cbfa7cc817853df903d170f4615c13aaff84e
therealemissions <me@liammartin.site>
2023-01-01 11:42:05 +0000

    updated types

--- Dependencies.ts
+++ Dependencies.ts
@@ -9,8 +9,12 @@ interface Inject {
   [t: string]: InjectWith;
 }
 
+const vars: Map<Dependency, string> = new Map();
+
 export function inject(type: Dependency) {
   return function (target: any, key: string): void {
+    console.log(target);
+    console.log(key);
     if (!Reflect.hasMetadata(key, target)) {
       Reflect.defineMetadata(key, {}, target);
     }
