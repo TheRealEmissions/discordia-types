e29dc7e679f4a098ec41afee02c6a7ba4f4ff530
therealemissions <me@liammartin.site>
2023-01-01 12:04:18 +0000

    updated

--- out/types/Dependencies.d.ts
+++
@@ -1,6 +0,0 @@
-import "reflect-metadata";
-import { Dependency } from "./Dependency.js";
-export interface InjectWith {
-    injectWith: Dependency | null;
-}
-export declare function inject(type: Dependency): (target: any, key: string) => void;
--- out/types/Types.d.ts
+++
@@ -1,3 +0,0 @@
-import { Dependency } from "./Dependency.js";
-import * as Dependencies from "./Dependencies.js";
-export { Dependency, Dependencies };
